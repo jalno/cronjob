ce600e6be8aea1f7c9163a3c5afca215fef8f287
Hossein Hosni <hosni.hossein@gmail.com>
2021-11-11 17:30:55 +0330

    Fix #8

--- libraries/tasks/task.php
+++ libraries/tasks/task.php
@@ -1,10 +1,17 @@
 <?php
 namespace packages\cronjob;
-use \packages\base\db;
-use \packages\base\date;
-use \packages\base\db\parenthesis;
-use \packages\base\db\dbObject;
-use \packages\cronjob\task\schedule;
+
+use packages\base\{Date, DB, DB\DBObject, DB\Parenthesis};
+use packages\cronjob\task\Schedule;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $process
+ * @property array|null $parameters
+ * @property "1"|"2" $status
+ * @property \packages\cronjob\Task\Schedule[] $schedules
+ */
 class task extends dbObject{
 	const active = 1;
 	const deactive = 2;
