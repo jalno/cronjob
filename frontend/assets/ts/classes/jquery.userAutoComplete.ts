8b283d581c6e0943fb2b53e6a709e762fccbccda
Hossein Hosni <hosni.hossein@gmail.com>
2024-11-06 20:17:10 +0330

    Use full version of jquery-ui instead of using single widget of it

--- frontend/assets/ts/classes/jquery.userAutoComplete.ts
+++ frontend/assets/ts/classes/jquery.userAutoComplete.ts
@@ -1,7 +1,8 @@
 /// <reference path="jquery.userAutoComplete.d.ts"/>
 
-import "jquery-ui/ui/widgets/autocomplete.js";
+import "jquery-ui/dist/jquery-ui.js";
 import {Router, webuilder} from "webuilder";
+
 interface user{
 	id:number;
 	name:string;
